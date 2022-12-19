408ecd67090204de3aacc6099103254603bf6211
MASAHIRO.YAMADA <masahiro.yamada@quest.co.jp>
2022-12-19 22:36:58 +0900

    初期資材-splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
+// Package imports:
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   runApp(const MyApp());
