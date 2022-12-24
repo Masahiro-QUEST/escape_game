e1869ed90acbaea07c39bf147b609c73a33fcd91
MASAHIRO.YAMADA <masahiro.yamada@quest.co.jp>
2022-12-25 00:36:56 +0900

    first_stageに円形画像を追加

--- lib/presentation/pages/game_page/escape_me_first_stage.dart
+++ lib/presentation/pages/game_page/escape_me_first_stage.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 class FirstStage extends StatelessWidget {
   const FirstStage({super.key});
+  final double _width = 100;
+  final double _height = 100;
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +13,18 @@ class FirstStage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Next page0'),
       ),
-      body: Center(child: Assets.images.firstStageOpen.image()),
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          curve: Curves.easeInOut,
+          width: _width,
+          height: _height,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Assets.images.firstStageOpen.image(),
+          ),
+        ),
+      ),
     );
   }
 }
